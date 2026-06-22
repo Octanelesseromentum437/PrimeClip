b0a3df388b3163b6af61a9c4a5258b3cd6faed1a
Luciano dii Souza <lucianodiisouza@hotmail.com>
2026-06-21 22:05:39 -0300

    feat(desktop): request notification permission when app loads

--- apps/desktop/src/components/GlobalJobMonitor.tsx
+++ apps/desktop/src/components/GlobalJobMonitor.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { useGlobalJobMonitor } from "../hooks/useJobStageNotifications";
+import { ensureNotificationPermission } from "../lib/notifications";
 
 export function GlobalJobMonitor() {
   useGlobalJobMonitor();
+
+  useEffect(() => {
+    void ensureNotificationPermission();
+  }, []);
+
   return null;
 }
